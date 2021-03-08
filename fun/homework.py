d21120b8c40b3ef0c65db44aef315037b4fd73f2
Alexis Daniels <danielsam3@vcu.edu>
2021-03-08 17:03:18 -0500

    change57

--- fun/homework.py
+++ fun/homework.py
@@ -2,17 +2,17 @@
 
 def find_greatest_number(incoming_list):
     return max(incoming_list)
-
+pass
   
 def find_least_number(incoming_list):
     return min(incoming_list)
-
+pass
      
 def add_list_numbers(incoming_list):
     return sum(incoming_list)
-
+pass
 
 def longest_value_key(incoming_dict):
     return len(incoming_dict)
-
+pass
      
