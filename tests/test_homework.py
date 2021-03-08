8acd645cf58e36c4a2b1a818ab3d24f2ea699b1e
Alexis Daniels <danielsam3@vcu.edu>
2021-03-08 17:19:34 -0500

    change61

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -2,22 +2,22 @@
 
 from fun import homework
 
-def test_greatest_number(incoming_list):
+def test_greatest_number():
     assert homework.find_greatest_number([1, 2, 3, 4, 5, 6, 7, 8]) == 8
     assert homework.find_greatest_number([7, 3, 21, 4, 1, 6]) == 21
 
-def test_least_number(incoming_list):
+def test_least_number():
     assert homework.find_least_number([1, 2, 3, 4, 5, 6, 7, 8]) == 1
     assert homework.find_least_number([8, 7, 3, 2, 4, 1, 6]) == 1
     
-def test_sum_of_list(incoming_list):
+def test_sum_of_list():
     assert homework.add_list_numbers([1, 2, 3, 4]) == 10
     assert homework.add_list_numbers([]) == 0
-    assert homework.add_list_numbers(None) == 0
+    assert homework.add_list_numbers(incoming_list) == 0
 
-def test_key_with_the_longest_value(incoming_dict):   
+def test_key_with_the_longest_value():   
     assert (
         homework.longest_value_key({"dog": "cat", "a": "asdfasdfasdfasdfasdf"} == "a")
     )
     assert homework.longest_value_key({}) is None
-    assert homework.longest_value_key(None) is None
+    assert homework.longest_value_key(incoming_dict) is None
