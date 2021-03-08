62bd872f079976f5021e04674e71780b036ce1a1
Alexis Daniels <danielsam3@vcu.edu>
2021-03-08 17:53:10 -0500

    change63

--- fun/homework.py
+++ fun/homework.py
@@ -1,18 +1,26 @@
 """Homework file for my students to have fun with some algorithms!"""
 
 def find_greatest_number(incoming_list):
+    """Required parameter, incoming_list, should be a list.
+    Find the largest number in the list."""
     return max(incoming_list)
 pass
   
 def find_least_number(incoming_list):
+    """Required parameter, incoming_list, should be a list.
+    Find the smallest/least number in the list."""
     return min(incoming_list)
 pass
      
 def add_list_numbers(incoming_list):
+    """Required parameter, incoming_list, should be a list.
+    Add all vlaues together and return it."""
     return sum(incoming_list)
 pass
 
 def longest_value_key(incoming_dict):
+    """Required parameter, incoming_dict, should be a dict.
+    Find the KEY that has a value with the highest length, use the len() function"""
     return len(incoming_dict)
 pass
      
