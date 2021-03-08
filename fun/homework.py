17367b0b4cb14aac41900f7acea3e76070e46067
Alexis Daniels <danielsam3@vcu.edu>
2021-03-08 07:35:20 -0500

    change39

--- fun/homework.py
+++ fun/homework.py
@@ -1,22 +1,22 @@
 """Homework file for my students to have fun with some algorithms!"""
 
-def find_greatest_number(incoming_list):
+def find_greatest_number():
     """Required parameter, incoming_list, should be a list.
         Find the largest number in the list."""
-find_value = max(incoming_list)
+find_value = max()
   
-def find_least_number(incoming_list):
+def find_least_number():
     """Required parameter, incoming_list, should be a list.
         Find the smallest/least number in the list."""
-find_value = min(incoming_list)
+find_value = min()
      
-def add_list_numbers(incoming_list):
+def add_list_numbers():
     """Required parameter, incoming_list, should be a list.
         Add all the values together and return it."""
-find_value = sum(incoming_list)
+find_value = sum()
 
-def longest_value_key(incoming_dict):
+def longest_value_key():
     """Required parameter, incoming_dict, should be a dict.
         Find the KEY that has a value with the highest length, use the len() function"""
-find_value = len(incoming_dict)
+find_value = len()
      
