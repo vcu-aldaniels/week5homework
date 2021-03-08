3c904c0d25a0162a20c6d7e8eeb76355782294bc
Alexis Daniels <danielsam3@vcu.edu>
2021-03-08 17:11:19 -0500

    change58

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -3,17 +3,14 @@
 from fun import homework
 
 def test_greatest_number():
-
     assert homework.find_greatest_number([1, 2, 3, 4, 5, 6, 7, 8]) == 8
     assert homework.find_greatest_number([7, 3, 21, 4, 1, 6]) == 21
 
 def test_least_number():
-
     assert homework.find_least_number([1, 2, 3, 4, 5, 6, 7, 8]) == 1
     assert homework.find_least_number([8, 7, 3, 2, 4, 1, 6]) == 1
     
 def test_sum_of_list():
-    
     assert homework.add_list_numbers([1, 2, 3, 4]) == 10
     assert homework.add_list_numbers([]) == 0
 
