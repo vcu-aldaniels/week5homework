1e27ae727615e57efe242d4653fa205e647f8202
Alexis Daniels <danielsam3@vcu.edu>
2021-03-08 19:18:14 -0500

    change66

--- fun/homework.py
+++ fun/homework.py
@@ -6,12 +6,14 @@ def find_greatest_number(incoming_list):
     return max(incoming_list)
 pass
   
+  
 def find_least_number(incoming_list):
     """Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list."""
     return min(incoming_list)
 pass
      
+
 def add_list_numbers(incoming_list):
     """Required parameter, incoming_list, should be a list.
     Add all vlaues together and return it."""
--- tests/test_homework.py
+++ tests/test_homework.py
@@ -7,16 +7,19 @@ def test_greatest_number():
     assert homework.find_greatest_number([1, 2, 3, 4, 5, 6, 7, 8]) == 8
     assert homework.find_greatest_number([7, 3, 21, 4, 1, 6]) == 21
 
+
 def test_least_number():
     """Quick test to see if you can find the smallest number in a list."""
     assert homework.find_least_number([1, 2, 3, 4, 5, 6, 7, 8]) == 1
     assert homework.find_least_number([8, 7, 3, 2, 4, 1, 6]) == 1
     
+
 def test_sum_of_list():
     """Quick test to see if you can add together all the numbers in a list."""
     homework.add_list_numbers([1, 2, 3, 4]) == 10
     homework.add_list_numbers([]) == 0
     homework.add_list_numbers(None) == 0
+
 
 def test_key_with_the_longest_value():
     """A little tricker. Which KEY has the 'longest' value associated with it?"""  
